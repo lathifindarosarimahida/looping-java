b5dd3a1b87be37a8bf9b9574f92aed259970044f
USER <USER@DESKTOP-LSF5PJH>
2018-08-02 12:09:52 +0700

    1 sampai 10

---
+++ src/jobsheet08/contoh2.java
@@ -0,0 +1,38 @@
+package jobsheet08;
+import java.util.Scanner;
+public class contoh2 {
+    public static void main(String[] args)
+    {
+        //PERULANGAN FOR
+        int x;
+        for(x=1; x <= 10; x++) 
+        {
+            System.out.print(x + " ");
+        }
+        
+        System.out.println();
+        
+        //PERULANGAN WHILE
+        x = 1;
+        while(x <= 10)
+        {
+            System.out.print(x + " ");
+            x++;
+        }
+        
+        //PERULANGAN DO WHILE
+        System.out.println();
+        
+        x = 1;
+        do
+        {
+            System.out.print(x +" ");
+            x++;
+        }
+        while(x <= 10);
+        {
+            
+        }
+    }
+}
+  
